22f4e02a1d0cef603bff70544e5bbc3f1342433e
NouraneRourou <49339753+NouraneRourou@users.noreply.github.com>
2020-01-12 13:42:36 +0100

    first commit registerservice Eureka

---
+++ src/test/java/com/example/registerserviceeureka/RegisterserviceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registerserviceeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterserviceEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
